43e1be2a8b849f708d4d8e2f35b29f33e56cf0a3
Abdelrahman Dwedar <dedou2005@gmail.com>
2022-05-20 22:51:23 +0200

    Setup prepschool headers
    * defined the new name of the file (1, 2)
    
    * Rename the class to PrepSchool to be more semitic (5)
    
    *  Add constructors (6, 7, 8)

--- headers/Prep-school.h
+++ headers/Prep-school.h
@@ -1,7 +1,11 @@
-#ifndef PRIMARY_SCHOOL_H
-#define PRIMARY_SCHOOL_H
+#ifndef PREP_SCHOOL_H
+#define PREP_SCHOOL_H
 #include "student.h"
 
-class Primary_school : public Student {};
+class PrepSchool : public Student {
+    public:
+        PrepSchool();
+        PrepSchool(std::string arg_name, int arg_age=7);
+};
 
 #endif
